d71cb9113fe29b15f66b4a9abea371030c3989db
Chenyun Zhang <morningchenyun@gmail.com>
2020-09-12 22:21:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 // Code your solution here
+let findMatching = (drivers, str) => drivers.filter( driver => driver.toLowerCase() === str.toLowerCase())
+let fuzzyMatch = (drivers, str) => drivers.filter(driver => (driver.split(""))[0] === str[0])
+let matchName = (drivers,str) => drivers.filter(driver => driver.name === str)
