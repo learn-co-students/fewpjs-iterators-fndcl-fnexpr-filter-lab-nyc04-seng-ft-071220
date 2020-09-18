9a1c9ed2ecf6e18eb8af92a67f78e076f05407a3
David Felix <david.felix02@hotmail.com>
2020-09-18 03:35:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(drivers,driver) {
+  return drivers.filter(person => person.toLowerCase() == driver.toLowerCase())
+}
+
+function fuzzyMatch(drivers,driver) {
+  // ((typeof person.name !== "undefined"&& typeof driver !== "undefined"))&&(person.name === driver || person.name[0] == driver[0]));
+  return drivers.filter(person => person.toLowerCase().indexOf(driver.toLowerCase()) === 0)
+}
+
+function matchName(drivers,driver) {
+  return drivers.filter(person => person.name === driver)
+}
